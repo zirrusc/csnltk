ee232ef40e4ba33c5b43b52a50e9fc1a5dd9b4b9
zirrusc <cloud.skyblue.0@gmail.com>
2013-07-26 19:20:30 +0900

    abc

---
+++ csnltk/csnltk/Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csnltk
+{
+	public static class Info
+	{
+		public const string Copyright = 
+			@"Copyright (C) 2001-2013 NLTK Project.
+
+			Distributed and Licensed under the Apache License, Version 2.0,
+			which is included by reference.";
+
+	}
+}
